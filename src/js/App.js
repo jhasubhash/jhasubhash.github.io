bc8b56a5b1bcfbd1e1a42bd962f408df00dd6b51
Subhash <sujha@adobe.com>
2020-12-14 18:10:28 +0530

    delay loading of component

--- src/js/App.js
+++ src/js/App.js
@@ -1,16 +1,16 @@
 import '../css/App.css';
-import React from 'react';
+import React, {useEffect} from 'react';
 import loadable from '@loadable/component'
 import 'react-typist/dist/Typist.css';
 import { faUser, faBriefcase, faCamera, faUserGraduate, faAddressBook} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Profile = loadable(() => import('./Profile'), {resolveComponent: (mod) => mod.Profile})
-const AboutMe = loadable(() => import('./AboutMe'), {resolveComponent: (mod) => mod.AboutMe});
-const Career = loadable(() => import('./Career'), {resolveComponent: (mod) => mod.Career});
-const Education = loadable(() => import('./Education'), {resolveComponent: (mod) => mod.Education})
-const Photos = loadable(() => import('./Photos'), {resolveComponent: (mod) => mod.Photos})
-const Contact = loadable(() => import('./Contact'), {resolveComponent: (mod) => mod.Contact})
+const Profile = loadable(() => import(/* webpackPrefetch: true */ './Profile'), {resolveComponent: (mod) => mod.Profile})
+const AboutMe = loadable(() => import(/* webpackPrefetch: true */ './AboutMe'), {resolveComponent: (mod) => mod.AboutMe});
+const Career = loadable(() => import(/* webpackPrefetch: true */ './Career'), {resolveComponent: (mod) => mod.Career});
+const Education = loadable(() => import(/* webpackPrefetch: true */ './Education'), {resolveComponent: (mod) => mod.Education})
+const Photos = loadable(() => import(/* webpackPrefetch: true */ './Photos'), {resolveComponent: (mod) => mod.Photos})
+const Contact = loadable(() => import(/* webpackPrefetch: true */ './Contact'), {resolveComponent: (mod) => mod.Contact})
 
 function App() {
   const [aboutMe, setAboutMe] = React.useState(false);
@@ -18,6 +18,7 @@ function App() {
   const [education, setEduction] = React.useState(false);
   const [camera, setCamera] = React.useState(false);
   const [contact, setContact] = React.useState(false);
+  
   const onIconClick = (id) => {
     if(id === 'aboutme'){
       setAboutMe(!aboutMe);
@@ -51,6 +52,15 @@ function App() {
       setContact(!contact);
     }
   }
+  useEffect(() => {
+    Profile.preload();
+    AboutMe.preload();
+    Career.preload();
+    Education.preload();
+    Photos.preload();
+    Contact.preload();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -60,7 +70,7 @@ function App() {
           </div>
         </div>
         <div className="iconList">
-          <FontAwesomeIcon icon={faUser} className="iconStyle" id="aboutme" style={{ transform : aboutMe ?"scale(0.80)":"scale(1)"}} onClick={(e)=>{onIconClick(e.currentTarget.id)}}/>
+          <FontAwesomeIcon icon={faUser} className="iconStyle" id="aboutme" style={{ transform : aboutMe ?"scale(0.80)":"scale(1)"}} onClick={(e)=>{onIconClick(e.currentTarget.id)}} onMouseOver={(e)=>{AboutMe.preload()}} />
           <FontAwesomeIcon icon={faUserGraduate} className="iconStyle" id="education" style={{ transform : education ?"scale(0.80)":"scale(1)"}} onClick={(e)=>{onIconClick(e.currentTarget.id)}}/>
           <FontAwesomeIcon icon={faBriefcase} className="iconStyle"  id="career" style={{ transform : career ?"scale(0.80)":"scale(1)"}} onClick={(e)=>{onIconClick(e.currentTarget.id)}}/>
           <FontAwesomeIcon icon={faCamera} className="iconStyle" id="camera" style={{ transform : camera ?"scale(0.80)":"scale(1)"}} onClick={(e)=>{onIconClick(e.currentTarget.id)}}/>
