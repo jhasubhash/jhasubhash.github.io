18caf59686eaf7ee5a172777830b7de1e86562bd
Subhash Jha <subhashjha@subhashs-MacBook-Pro-2.local>
2021-02-15 12:22:27 +0530

    Updates

--- static/js/8.24e3ebbb.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpjhasubhash.github.io"]=this["webpackJsonpjhasubhash.github.io"]||[]).push([[8],{100:function(i,s,t){},106:function(i,s,t){"use strict";t.r(s),t.d(s,"Life",(function(){return u}));var n=t(3),u=(t(100),function(){return Object(n.jsx)("div",{className:"Life"})})}}]);
-//# sourceMappingURL=8.24e3ebbb.chunk.js.map
