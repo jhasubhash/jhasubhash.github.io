fb491bac85b43db7b7ec55c49c0c3c42577d7095
Subhash Jha <sujha@adobe.com>
2021-06-14 16:27:05 +0530

    adding social links

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 import code from "../assets/images/code.json";
 import { useLottie } from "lottie-react";
 
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
 interface Props {
     
 }
@@ -34,9 +39,27 @@ const useStyles = makeStyles((theme: Theme) =>
         '@media (max-width: 500px)': {
             justifyContent: 'flex-end',
         },
+    },
+    social: {
+        //marginTop: 'auto',
+        display: 'flex',
+        padding: '6vh',
+        gap:'2vh'
+    },
+    iconLink: {
+        width:'25px',
+        height:'25px',
+        cursor:'pointer',
+        display:'block',
+        textDecoration: 'none',
+        color:'white'
     }
   }),
 );
+
+const isDesktop = () => {
+    return window.innerWidth > 800;
+}
 
 const GetCodeAnim = () => {
     const options = {
@@ -53,11 +76,28 @@ export const Main = (props: Props) => {
     return (
         <Paper className={classes.content} square>
             <div className={classes.profile}>
+                {!isDesktop() &&
+                <React.Fragment>
                 <Typography variant='h6'>Hello World !!! </Typography>
                 <br/>
-                <Typography variant='h5'>I am Subhash Jha</Typography>
-                <Typography variant='body2'>Software Developer</Typography>
-                <Typography variant='caption'>C++ | Javascript | ReactJs</Typography>
+                <Typography variant='h4'>I am Subhash Jha</Typography>
+                <Typography variant='body1'>Software Developer</Typography>
+                <Typography variant='body2'>C++ | Javascript | ReactJs</Typography>
+                </React.Fragment>}
+                {isDesktop() &&
+                <React.Fragment>
+                <Typography variant='h5'>Hello World !!! </Typography>
+                <br/>
+                <Typography variant='h3'>I am Subhash Jha</Typography>
+                <Typography variant='h6'>Software Developer</Typography>
+                <Typography variant='body1'>C++ | Javascript | ReactJs</Typography>
+                </React.Fragment>}
+                <div className={classes.social}>
+                    <a className={classes.iconLink} href="https://github.com/pseudocoder-in" target={"_blank"} rel="noreferrer"><GitHubIcon/></a>
+                    <a className={classes.iconLink} href="https://twitter.com/subhashjha333" target={"_blank"} rel="noreferrer"><TwitterIcon/></a>
+                    <a className={classes.iconLink} href="https://www.linkedin.com/in/scjha/" target={"_blank"} rel="noreferrer"><LinkedInIcon/></a>
+                    <a className={classes.iconLink} href="mailto:subhashjha.mail@gmail.com"><EmailIcon/></a>
+                </div>
             </div>
             <div className={classes.codeAnim}>
             <GetCodeAnim/>
