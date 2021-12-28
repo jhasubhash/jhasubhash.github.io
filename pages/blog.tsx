b6f0bc19432626060a42c0b708d4152b983fb374
Subhash Jha <subhashjha.mail@gmail.com>
2021-12-28 14:06:59 +0530

    pagination support

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -4,10 +4,17 @@ import matter from "gray-matter";
 import Post from "../components/Post";
 import {sortByDate} from "../utils";
 import Link from "next/Link";
-
+import Pagination from 'next-pagination'
+import 'next-pagination/dist/index.css'
+import { useRouter } from 'next/router'
+let page = 1;
+let size = 20;
 function Blog({ posts }) {
+  const router = useRouter();
+  page = router.query.page ? Number(router.query.page) : page;
+  size = router.query.size ? Number(router.query.size) : size;
+  console.log(page+" "+size+" "+posts.length);
   return (<>
-  
   <Link href='/'>
         <div className="btn btn-back">Back</div>
     </Link>
@@ -16,6 +23,8 @@ function Blog({ posts }) {
         <Post post={post} key={index}/>
       ))}
     </div>
+    <Pagination total={posts.length} />
+    <br/>
     <Link href='/'>
         <div className="btn btn-back">Back</div>
     </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Subhash Jha
+          I am Subhash Jha
         </h1>
 
         <p className={styles.description}>
