96698f262f06c2e5e30dddcc93dd5706f262a6dd
Subhash Jha <subhashjha.mail@gmail.com>
2021-12-28 18:22:18 +0530

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "76490541a99cfd50db3c"; }
+/******/ 		__webpack_require__.h = function() { return "24da79fa40558d33db60"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
